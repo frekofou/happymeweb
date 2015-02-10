e450fd00eeb15640d9d02719b1ceb87a863e190c
frekofou <frederic.marianne@gmail.com>
2015-02-10 01:29:37 +0100

    commentaires

--- happy-service/src/main/resources/assets/app/js/map/map-controller.js
+++ happy-service/src/main/resources/assets/app/js/map/map-controller.js
@@ -4,26 +4,34 @@ angular.module('happy')
 
 
 .controller("MapController", function($scope,$log, $timeout) {
+    // initialisation de la map
     $scope.map = {center: {latitude:  -21.040414446125766, longitude:  55.718346697807306}, zoom: 20 };
     $scope.options = {scrollwheel: false};
+    // compteurs de modification
     $scope.coordsUpdates = 0;
     $scope.dynamicMoveCtr = 0;
     var service;
      var geocoder;
     var latlng; 
     
+    // creation du marker
     $scope.marker = {        
       id: 0,
+        // coordonnées de positionnement du marker : to do geolocaliser
       coords: {
         latitude:  -21.040414446125766,
         longitude: 55.718346697807306
       },
       options: { draggable: true },
+        // evenement relachement du drag du marker
       events: {
+          // le marker, le fait de drager, les evenements navigateur
           dragend: function (marker, eventName, args) {
           $log.log('marker dragend');
+            // position du marker sur la map --> le marker porte sa position et non la map  
           var lat = marker.getPosition().lat();
           var lon = marker.getPosition().lng();
+              // a supprime?
           geocoder = new google.maps.Geocoder();
           latlng = new google.maps.LatLng(lat, lon);
           $log.log(lat);
@@ -32,26 +40,29 @@ angular.module('happy')
 
           $scope.marker.options = {
             draggable: true,
-            labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-            labelAnchor: "100 0"
+              // fix me car ne s'affiche pas
+            labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude
+            //labelAnchor: "100 0"
             
           };
-           
+          // peut mieux faire pour lancer le code 
          codeAddress();
          function codeAddress() {
                 $log.log('dans le codeAddress');
             var infowindow = new google.maps.InfoWindow();
                 $log.log('info window ok');
                       
-                    
+               // recuperation des information via l'api geocoder   new google.maps.Geocoder();
+             // latLng issue de la position du marker, results contient la réponse de l'API, status contient le OK/KO
            geocoder.geocode({'latLng': latlng}, function(results, status) {
-               $log.log('dans geocode');
+               $log.log('tentative de geocoding via ' + latlng);
                       if (status == google.maps.GeocoderStatus.OK) {
                           $log.log('status == google.maps.GeocoderStatus.OK');
                         if (results[1]) {
-                            $log.log('geocode OK');
+                            $log.log('geocoding réussi');
+                            $log.log('reponse brute:');
                             $log.log(results[0]);
-                            $log.log(results[1].formatted_address);
+                            $log.log('premier element:' + results[1].formatted_address);
                          
                         }
                       } else {
